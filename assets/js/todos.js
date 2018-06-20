b285108ede0df6daa1289eeb6a62b9b1cab19d5a
Oscar <strange2155@yahoo.com>
2018-06-19 22:55:48 -0500

    Adding and Deleting function working

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,15 +1,25 @@
 
 //check off specific todos by clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed")
 });
 
 //click on x to delete // TODO:
 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500,function(){
     $(this).remove();
   });
   event.stopPropagation();
 });
+
+$("input[type='text'").keypress(function(event){
+  if(event.which === 13){
+    // grabbing new to do text from input
+  var todoText = $(this).val();
+  $(this).val("");
+  // create a new li and add to ul
+  $("ul").append("<li><span>X</span> " + todoText + "</li>")
+  }
+})
